c2010eef154afe2bd185d22508db0d8147f4c498
Meet Trivedi <116267381+TrivediMeet@users.noreply.github.com>
2025-01-26 16:19:44 +0530

    add relation and add functionality such as go to site and all

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, serial, text, varchar, integer, boolean } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  varchar,
+  integer,
+  boolean,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export const projects = pgTable("projects", {
@@ -9,3 +16,24 @@ export const projects = pgTable("projects", {
   userId: varchar("user_id"),
 });
 
+export const projectsRelations = relations(projects, ({ many }) => ({
+  feedbacks: many(feedbacks),
+}));
+
+
+export const feedbacks = pgTable("feedbacks", {
+  id: serial("id").primaryKey(),
+  projectId: integer("project_id"),
+  userName: text("user_name"),
+  userEmail: text("user_email"),
+  message: text("message"),
+  // rating: integer("rating"),
+
+});
+
+export const feedbacksRelations = relations(feedbacks, ({ one }) => ({
+  project: one(projects, {
+    fields: [feedbacks.projectId],
+    references: [projects.id],
+  }),
+}));
